2fef1547b6a88515e201c1981d51659865979625
yllberisha1 <yllberisha65@gmail.com>
2026-03-14 11:45:52 +0100

    y5y
    aasxxX

--- reactnatvie1/App.js
+++ reactnatvie1/App.js
@@ -1,16 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigation} from '@react-navigation/native';
+import { createStackNavigator} from '@react-navigation/stack';
 import MainScreen from './screens/MainScreen';  
+import ExerciseScreen from './screens/ExerciseScreen';
 
-const Stack = createStackNavigation();
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name="Main" component={MainScreen} />
+        <Stack.Screen name="Exercise" component={ExerciseScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
---
+++ reactnatvie1/screens/ExerciseScreen.js
@@ -0,0 +1,13 @@
+import React  from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+const ExeciseScreen = () => {
+    let message = "This is the Exercise Screen";
+    message = "hi there "
+    return (
+        <Text>{message}</Text>
+    )
+}
+
+export default ExeciseScreen;
+
